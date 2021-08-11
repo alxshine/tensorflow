c6da132bf13c6807d004c507ad2841ec35e3b435
Alexander Schlögl <alexander.schloegl@uibk.ac.at>
2021-08-11 19:50:14 +0000

    finish run script

--- forennsic/run.py
+++ forennsic/run.py
@@ -1,2 +1,9 @@
 import tensorflow as tf
+import numpy as np
 
+with tf.device("/device:CPU:0"):
+  model = tf.keras.models.load_model("forennsic/minimal.h5")
+  sample = np.load("forennsic/sample.npy")
+
+  predictions = model(sample)
+  print(predictions)
